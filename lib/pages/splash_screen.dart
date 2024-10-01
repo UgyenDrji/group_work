564669873502b1ec31dc0c0169fb24fd574bf82c
Ugyen Dorji <ugyennn345@gmail.com>
2024-10-01 09:20:16 +0600

    Initial commit

---
+++ lib/pages/splash_screen.dart
@@ -0,0 +1,65 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:untitled/constans.dart';
+import 'package:untitled/pages/registration%20page.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Timer(
+        Duration(seconds: 3),
+        () => Navigator.of(context).pushAndRemoveUntil(
+            MaterialPageRoute(builder: (builder) => RegistrationPage()),
+            (route) => false));
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: 100,
+                height: 100,
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                        image: AssetImage("assets/image/logo1.png"),
+                        fit: BoxFit.cover)),
+              ),
+              Lottie.asset('assets/animation/splash.json'),
+              RichText(
+                text: TextSpan(
+                    style: myStyle(26, primaryColor, FontWeight.bold),
+                    children: [
+                      TextSpan(
+                        text: "Job",
+                      ),
+                      TextSpan(
+                          text: "Hunt",
+                          style: myStyle(26, Colors.orange, FontWeight.bold)),
+                      TextSpan(
+                        text: "Go!",
+                        style: myStyle(26, Colors.blue, FontWeight.bold),
+                      ),
+                    ]),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
